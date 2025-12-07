98d6beba9de216a35cbf4671382fc8c83ad16c98
guilhem-alb <guilhem.alb.github@gmail.com>
2025-12-07 12:26:08 +0100

    modified db schema: updated relevant scripts

--- scripts/phase1_sqlite/create_schema.py
+++ scripts/phase1_sqlite/create_schema.py
@@ -3,165 +3,194 @@ import sqlite3
 with sqlite3.connect("../../data/imdb.db") as conn:
     c = conn.cursor()
 
-    # Table Cast
+    conn.execute("BEGIN TRANSACTION")
 
-    c.execute("""
-    CREATE TABLE cast (
-    movie_id VARCHAR(20),
-    person_id VARCHAR(20),
-    character_id INTEGER,
-              
-    PRIMARY KEY (movie_id, person_id, character_id),
-    FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
-    FOREIGN KEY (person_id) REFERENCES persons(person_id),
-    FOREIGN KEY (character_id) REFERENCES characters(character_id)
-    );
-    """)
+    try:
+        # Table Cast
 
-    # Table Characters
-
-    c.execute("""
-    CREATE TABLE characters (
-    character_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(100) UNIQUE NOT NULL COLLATE NOCASE
-    )    
-    """)
-
-    # Table movie_genres
-
-    c.execute("""
-    CREATE TABLE movie_genres (
-    movie_id VARCHAR(20),
-    genre_id INTEGER,
-              
-    PRIMARY KEY (movie_id, genre_id),
-    FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
-    FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
-    )
-    """)
-
-    # Table Genres
-
-    c.execute("""
-    CREATE TABLE genres (
-    genre_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    genre VARCHAR(50) UNIQUE NOT NULL COLLATE NOCASE
-    )
-    """)
-
-    # Table knownformovies
-
-    c.execute("""
-    CREATE TABLE known_for_movies (
-    person_id VARCHAR(20),
-    movie_id VARCHAR(20),
-              
-    PRIMARY KEY (person_id, movie_id),
-    FOREIGN KEY (person_id) REFERENCES persons(person_id),        
-    FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
-    )
-    """)
-
-    # Table Movies
-
-    c.execute("""
-    CREATE TABLE movies (
-        movie_id VARCHAR(20) PRIMARY KEY,
-        isAdult BOOLEAN,
-        startYear INTEGER,
-        endYear INTEGER,
-        runtimeMinutes INTEGER,
-              
-        CHECK(startYear >= 1895),
-        CHECK(endYear >= 1895 AND endYear >= startYear),
-        CHECK(runtimeMinutes > 0)
-    )
-    """)
-
-    # Table Persons
-
-    c.execute("""
-    CREATE TABLE persons (
-        person_id VARCHAR(20) PRIMARY KEY,
-        primaryName VARCHAR(100),
-        birthYear INTEGER,
-        deathYear INTEGER,
-              
-        CHECK(birthYear > 1800),
-        CHECK(deathYear > 1850 AND deathYear >= birthYear)
-    )   
-    """)
-
-    # Table Principals
-
-    c.execute("""
-    CREATE TABLE principals (
+        c.execute("""
+        CREATE TABLE cast (
         movie_id VARCHAR(20),
-        ordering INTEGER,
         person_id VARCHAR(20),
-        profession_id INTEGER,
-        category VARCHAR(100),
-              
-        PRIMARY KEY (movie_id, ordering),
+        character_id INTEGER,
+                
+        PRIMARY KEY (movie_id, person_id, character_id),
         FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
         FOREIGN KEY (person_id) REFERENCES persons(person_id),
-        FOREIGN KEY (profession_id) REFERENCES professions(profession_id)
-    )
-    """)
+        FOREIGN KEY (character_id) REFERENCES characters(character_id)
+        );
+        """)
 
-    # Table person_profession
+        # Table Characters
 
-    c.execute("""
-    CREATE TABLE person_profession (
-    person_id VARCHAR(20),
-    profession_id INTEGER,
-              
-    PRIMARY KEY (person_id, profession_id),
-    FOREIGN KEY (person_id) REFERENCES persons(person_id),
-    FOREIGN KEY (profession_id) REFERENCES professions(profession_id)
-    )   
-    """)
+        c.execute("""
+        CREATE TABLE characters (
+        character_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(100) UNIQUE NOT NULL COLLATE NOCASE
+        )    
+        """)
 
-    # Table professions
+        # Table movie_genres
 
-    c.execute("""
-    CREATE TABLE professions (
-        profession_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        jobName VARCHAR(100) UNIQUE NOT NULL COLLATE NOCASE
-    )
-    """)
-
-    # Table ratings
-
-    c.execute("""
-    CREATE TABLE ratings (
-        movie_id VARCHAR(20) PRIMARY KEY,
-        averageRating REAL,
-        numVotes INTEGER,
-              
-        FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
-        
-        CHECK(averageRating >= 0 AND averageRating <= 10),
-        CHECK(numVotes >= 0)
-    )
-    """)
-
-    # Table titles
-
-    c.execute("""
-    CREATE TABLE titles (
+        c.execute("""
+        CREATE TABLE movie_genres (
         movie_id VARCHAR(20),
-        ordering INTEGER,
-        title VARCHAR(200),
-        region VARCHAR(20),
-        language VARCHAR(20),
-        types VARCHAR(100),
-        attributes VARCHAR(100),
-        isOriginal BOOLEAN,
-        isPrimary BOOLEAN,
-              
-        PRIMARY KEY (movie_id, ordering),
-        FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
-    )
-    """)
+        genre_id INTEGER,
+                
+        PRIMARY KEY (movie_id, genre_id),
+        FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
+        FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+        )
+        """)
 
+        # Table Genres
+
+        c.execute("""
+        CREATE TABLE genres (
+        genre_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        genre_name VARCHAR(50) UNIQUE NOT NULL COLLATE NOCASE
+        )
+        """)
+
+        # Table knownformovies
+
+        c.execute("""
+        CREATE TABLE known_for_movies (
+        person_id VARCHAR(20),
+        movie_id VARCHAR(20),
+                
+        PRIMARY KEY (person_id, movie_id),
+        FOREIGN KEY (person_id) REFERENCES persons(person_id),        
+        FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
+        )
+        """)
+
+        # Table Movies
+
+        c.execute("""
+        CREATE TABLE movies (
+            movie_id VARCHAR(20) PRIMARY KEY,
+            year INTEGER,
+            runtime_minutes INTEGER,
+                
+            CHECK(year >= 1895),
+            CHECK(runtime_minutes > 0)
+        )
+        """)
+
+        # Table Persons
+
+        c.execute("""
+        CREATE TABLE persons (
+            person_id VARCHAR(20) PRIMARY KEY,
+            name VARCHAR(100),
+            birth_year INTEGER,
+            death_year INTEGER,
+                
+            CHECK(birth_year > 1800),
+            CHECK(death_year > 1850 AND death_year >= birth_year)
+        )   
+        """)
+
+        # Table Principals
+
+        c.execute("""
+        CREATE TABLE principals (
+            movie_id VARCHAR(20),
+            ordering INTEGER,
+            person_id VARCHAR(20),
+            profession_id INTEGER,
+            job VARCHAR(100),
+                
+            PRIMARY KEY (movie_id, ordering),
+            FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
+            FOREIGN KEY (person_id) REFERENCES persons(person_id),
+            FOREIGN KEY (profession_id) REFERENCES professions(profession_id)
+        )
+        """)
+
+        # Table person_profession
+
+        c.execute("""
+        CREATE TABLE person_profession (
+        person_id VARCHAR(20),
+        profession_id INTEGER,
+                
+        PRIMARY KEY (person_id, profession_id),
+        FOREIGN KEY (person_id) REFERENCES persons(person_id),
+        FOREIGN KEY (profession_id) REFERENCES professions(profession_id)
+        )   
+        """)
+
+        # Table professions
+
+        c.execute("""
+        CREATE TABLE professions (
+            profession_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            job_name VARCHAR(100) UNIQUE NOT NULL COLLATE NOCASE
+        )
+        """)
+
+        # Table ratings
+
+        c.execute("""
+        CREATE TABLE ratings (
+            movie_id VARCHAR(20) PRIMARY KEY,
+            average_rating REAL,
+            num_votes INTEGER,
+                
+            FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
+            
+            CHECK(average_rating >= 0 AND average_rating <= 10),
+            CHECK(num_votes >= 0)
+        )
+        """)
+
+        # Table title_ordering
+
+        c.execute("""
+        CREATE TABLE title_ordering (
+            movie_id VARCHAR(20),
+            title_id INTEGER,
+            ordering INTEGER,
+                  
+            region VARCHAR(20),
+            language VARCHAR(20),
+            types VARCHAR(200),
+            attributes VARCHAR(200),
+        
+            PRIMARY KEY (movie_id, title_id, ordering),
+            FOREIGN KEY (movie_id, title_id) REFERENCES movie_titles(movie_id, title_id)
+        )
+        """)
+
+        # Table movie_titles
+
+        c.execute("""
+        CREATE TABLE movie_titles (
+            movie_id VARCHAR(20),
+            title_id INTEGER,
+            is_primary BOOLEAN,
+            is_original BOOLEAN,
+                
+            PRIMARY KEY (movie_id, title_id),
+            FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
+            FOREIGN KEY (title_id) REFERENCES titles(title_id)
+        )
+        """)
+
+        # Table titles
+
+        c.execute("""
+        CREATE TABLE titles (
+            title_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title_name VARCHAR(200) UNIQUE NOT NULL COLLATE NOCASE 
+        )
+        """)
+
+        conn.commit()
+        
+    except sqlite3.Error as e:
+        print("base de donnée non crée, cause:", e)
+        conn.rollback()
+    
--- scripts/phase1_sqlite/import_data.py
+++ scripts/phase1_sqlite/import_data.py
@@ -102,7 +102,7 @@ def import_genres(conn):
                 continue
             try:
                 c.execute("""
-                    INSERT INTO genres (genre)
+                    INSERT INTO genres (genre_name)
                     VALUES (?)
                 """, (
                     elt,
@@ -141,7 +141,7 @@ def import_movie_genres(conn):
             try:
                 c.execute("""
                     INSERT INTO movie_genres(movie_id, genre_id)
-                    VALUES(?, (SELECT genre_id FROM genres WHERE genre = ? COLLATE NOCASE LIMIT 1))
+                    VALUES(?, (SELECT genre_id FROM genres WHERE genre_name = ? COLLATE NOCASE LIMIT 1))
                 """, (
                     row["mid"],
                     row["genre"]
@@ -213,13 +213,11 @@ def import_movies(conn):
         for _, row in moviesDf.iterrows():
             try:
                 c.execute("""
-                    INSERT INTO movies (movie_id, isAdult, startYear, endYear, runtimeMinutes)
-                    VALUES (?, ?, ?, ?, ?)
+                    INSERT INTO movies (movie_id, year, runtime_minutes)
+                    VALUES (?, ?, ?)
                 """, (
                     row["mid"],
-                    bool(row["isAdult"]) if not pd.isna(row["isAdult"]) else None,
                     int(row["startYear"]) if not pd.isna(row["startYear"]) else None,
-                    int(row["endYear"]) if not pd.isna(row["endYear"]) else None,
                     int(row["runtimeMinutes"]) if not pd.isna(row["runtimeMinutes"]) else None,
                 ))
                 insert_cnt += 1
@@ -252,7 +250,7 @@ def import_persons(conn):
         for _, row in personsDf.iterrows():
             try:
                 c.execute("""
-                    INSERT INTO persons (person_id, primaryName, birthYear, deathYear)
+                    INSERT INTO persons (person_id, name, birth_year, death_year)
                     VALUES (?, ?, ?, ?)
                 """, (
                     row["pid"],
@@ -293,7 +291,7 @@ def import_professions(conn):
                 continue
             try:
                 c.execute("""
-                    INSERT INTO professions (jobName)
+                    INSERT INTO professions (job_name)
                     VALUES (?)
                 """, (
                     elt,
@@ -331,7 +329,7 @@ def import_person_profession(conn):
             try:
                 c.execute("""
                     INSERT INTO person_profession(person_id, profession_id)
-                    VALUES(?, (SELECT profession_id FROM professions WHERE jobName = ? COLLATE NOCASE LIMIT 1))
+                    VALUES(?, (SELECT profession_id FROM professions WHERE job_name = ? COLLATE NOCASE LIMIT 1))
                 """, (
                     row["pid"],
                     row["jobName"]
@@ -366,7 +364,7 @@ def import_ratings(conn):
         for _, row in ratingsDf.iterrows():
             try:
                 c.execute("""
-                    INSERT INTO ratings (movie_id, averageRating, numVotes)
+                    INSERT INTO ratings (movie_id, average_rating, num_votes)
                     VALUES (?, ?, ?)
                 """, (
                     row["mid"],
@@ -387,18 +385,240 @@ def import_ratings(conn):
         conn.rollback()
         print("!!! TRANSACTION Echouée !!!:\n", e)
 
-# Depends on table movies
+# No dependencies
 def import_titles(conn):
     TRANSAC_NAME = "titles"
     insert_cnt = 0
     failure_cnt = 0
 
     c = conn.cursor()
+    titlesDf = pd.read_csv(CSV_REPO + "titles.csv")
+    moviesDf = pd.read_csv(CSV_REPO + "movies.csv")
 
-    # création d'une lookup table pour éviter de faire un SELECT par ligne, la tabel titles étant très grosse
+    titlesSerie = pd.concat([
+        titlesDf["title"],
+        moviesDf["originalTitle"],
+        moviesDf["primaryTitle"]
+    ]).drop_duplicates()
+
+    try:
+        print("--------------TRANSACTION " + TRANSAC_NAME + "--------------")
+        conn.execute("BEGIN TRANSACTION")
+
+        for _, elt in titlesSerie.items():
+            try:
+                c.execute("""
+                    INSERT INTO titles (title_name)
+                    VALUES (?)
+                """, (
+                    elt,
+                ))
+                insert_cnt += 1
+            except sqlite3.IntegrityError as e:
+                # Skip invalid rows
+                failure_cnt += 1
+
+        conn.commit()
+        print("Transaction " + TRANSAC_NAME + " terminée avec succès: ")
+        print("Lignes insérées: ", insert_cnt)
+        print("Lignes abandonnées: ", failure_cnt)
+
+    except sqlite3.Error as e:
+        conn.rollback()
+        print("!!! TRANSACTION Echouée !!!:\n", e)
+
+# Depends on movies and titles:
+def import_movie_title(conn):
+    TRANSAC_NAME = "movie_titles"
+    insert_from_titles = 0
+    failure_from_titles = 0
+
+    c = conn.cursor()
+
+    # création d'une lookup table pour éviter de faire un SELECT par ligne, la table titles étant très grosse
     moviesDf = pd.read_csv(CSV_REPO + "movies.csv")
     primaryLUT = set(zip(moviesDf["mid"], moviesDf["primaryTitle"]))
-                     
+    originalLUT = set(zip(moviesDf["mid"], moviesDf["originalTitle"]))
+
+    titlesDf = pd.read_csv(CSV_REPO + "titles.csv")
+    titlesLUT = set(zip(titlesDf["mid"], titlesDf["title"]))
+    try:
+
+        # Complete with table movies
+        print("--------------TRANSACTION " + TRANSAC_NAME + "--------------")
+        conn.execute("BEGIN TRANSACTION")
+
+        for _, row in titlesDf.iterrows():
+            try:
+                c.execute("""
+                    INSERT INTO movie_titles (movie_id, title_id, is_primary, is_original)
+                    VALUES (?, 
+                        (SELECT title_id FROM titles WHERE title_name = ? COLLATE NOCASE),
+                        ?, ?)
+                """, (
+                    row["mid"],
+                    row["title"],
+                    int((row["mid"], row["title"]) in primaryLUT),
+                    int(bool(row["isOriginalTitle"]) or ((row["mid"], row["title"]) in originalLUT))
+                ))
+                insert_from_titles += 1
+            except sqlite3.IntegrityError as e:
+                # Skip invalid rows
+                failure_from_titles += 1
+
+        insert_from_movies = 0
+        failure_from_movies = 0
+        # populate with the titles in movies as well for completeness
+        for _, row in moviesDf.iterrows():
+            try:
+                # store in variables to avoid looking up multiple times
+                mid = row["mid"]
+                # skip NaN or empty strings
+                originalTitle = row["originalTitle"] if pd.notna(row["originalTitle"]) and row["originalTitle"] != "" else None
+                primaryTitle  = row["primaryTitle"]  if pd.notna(row["primaryTitle"])  and row["primaryTitle"]  != "" else None
+
+
+                # No need to update existing values, as boolean fields are already completed
+                insertOriginal = False if (originalTitle is not None) and (mid, originalTitle) in titlesLUT else True
+                insertPrimary = False if (primaryTitle is not None) and (mid, primaryTitle) in titlesLUT else True
+
+                # Si les titres sont les mêmes, on insère une seule fois 
+                if insertOriginal and insertPrimary and originalTitle == primaryTitle:
+                    c.execute("""
+                        INSERT INTO movie_titles (movie_id, title_id, is_original, is_primary)
+                        VALUES (?, 
+                            (SELECT title_id FROM titles WHERE title_name = ? COLLATE NOCASE),
+                            1, 1)
+                    """, (
+                        mid,
+                        originalTitle
+                    ))
+
+                    insert_from_movies += 1
+                    continue
+                if insertOriginal:
+                    c.execute("""
+                        INSERT INTO movie_titles (movie_id, title_id, is_original, is_primary)
+                        VALUES (?, 
+                            (SELECT title_id FROM titles WHERE title_name = ? COLLATE NOCASE),
+                            1, 0)
+                    """, (
+                        mid,
+                        originalTitle
+                    ))
+                    insert_from_movies += 1
+                if insertPrimary:
+                    c.execute("""
+                    INSERT INTO movie_titles (movie_id, title_id, is_original, is_primary)
+                        VALUES (?, 
+                            (SELECT title_id FROM titles WHERE title_name = ? COLLATE NOCASE),
+                            0, 1)
+                """, (
+                    mid,
+                    primaryTitle
+                ))
+                    insert_from_movies += 1
+            except sqlite3.IntegrityError as e:
+                # Skip invalid rows
+                failure_from_movies += 1
+
+        #----------Nettoyage----------
+
+        # Si plusieurs titres primaires ou secondaires: en garder 1 seul
+        c.execute("""
+            UPDATE movie_titles
+            SET is_primary = 0
+            WHERE rowid NOT IN (
+                SELECT MIN(rowid)
+                FROM movie_titles
+                WHERE is_primary = 1
+                GROUP BY movie_id
+            )
+            AND is_primary = 1;
+        """)
+
+        c.execute("""
+            UPDATE movie_titles
+            SET is_original = 0
+            WHERE rowid NOT IN (
+                SELECT MIN(rowid)
+                FROM movie_titles
+                WHERE is_original = 1
+                GROUP BY movie_id
+            )
+            AND is_original = 1;
+        """)
+
+        # Si une seule des deux valeurs manquante: garder la deuxième
+        c.execute("""
+            UPDATE movie_titles
+            SET is_original = 1
+            WHERE rowid IN (
+                SELECT mt1.rowid
+                FROM movie_titles mt1
+                WHERE mt1.is_primary = 1
+                AND mt1.movie_id IN (
+                    SELECT movie_id
+                    FROM movie_titles
+                    GROUP BY movie_id
+                    HAVING SUM(is_original) = 0
+                )
+            );
+        """)
+        c.execute("""
+            UPDATE movie_titles
+            SET is_primary = 1
+            WHERE rowid IN (
+                SELECT mt1.rowid
+                FROM movie_titles mt1
+                WHERE mt1.is_original = 1
+                AND mt1.movie_id IN (
+                    SELECT movie_id
+                    FROM movie_titles
+                    GROUP BY movie_id
+                    HAVING SUM(is_primary) = 0
+                )
+            );
+        """)
+
+        # Si les deux valeurs manquantes: prendre un titre au hasard
+        c.execute("""
+            UPDATE movie_titles
+            SET is_primary = 1,
+                is_original = 1
+            WHERE rowid IN (
+                SELECT rowid
+                FROM movie_titles
+                WHERE movie_id IN (
+                    SELECT movie_id
+                    FROM movie_titles
+                    GROUP BY movie_id
+                    HAVING SUM(is_primary) = 0 AND SUM(is_original) = 0
+                )
+                GROUP BY movie_id
+            );
+        """)
+
+        conn.commit()
+        print("Transaction " + TRANSAC_NAME + " terminée avec succès: ")
+
+        print("Lignes insérées depuis titles.csv: ", insert_from_titles)
+        print("Lignes abandonnées depuis titles.csv: ", failure_from_titles)
+        
+        print("\nLignes insérées depuis movies.csv: ", insert_from_movies)
+        print("Lignes abandonnées depuis movies.csv: ", failure_from_movies)
+
+    except sqlite3.Error as e:
+        conn.rollback()
+        print("!!! TRANSACTION Echouée !!!:\n", e)
+
+# Depends on movie_titles
+def import_title_ordering(conn):
+    TRANSAC_NAME = "title_ordering"
+    insert_cnt = 0
+    failure_cnt = 0
+
+    c = conn.cursor()
     titlesDf = pd.read_csv(CSV_REPO + "titles.csv")
 
     try:
@@ -408,30 +628,19 @@ def import_titles(conn):
         for _, row in titlesDf.iterrows():
             try:
                 c.execute("""
-                    INSERT INTO titles (
-                        movie_id,
-                        ordering,
-                        title,
-                        region,
-                        language,
-                        types,
-                        attributes,
-                        isOriginal,
-                        isPrimary
-                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    INSERT INTO title_ordering (movie_id, title_id, ordering, region, language, types, attributes)
+                    VALUES (?, 
+                        (SELECT title_id FROM titles WHERE title_name = ? COLLATE NOCASE),
+                        ?, ?, ?, ?, ?)
                 """, (
                     row["mid"],
+                    row["title"],
                     row["ordering"],
-                    row["title"] if not pd.isna(row["title"]) and not row["title"] == "" else None,
                     row["region"] if not pd.isna(row["region"]) and not row["region"] == "" else None,
                     row["language"] if not pd.isna(row["language"]) and not row["language"] == "" else None,
                     row["types"] if not pd.isna(row["types"]) and not row["types"] == "" else None,
-                    row["attributes"] if not pd.isna(row["attributes"]) and not row["attributes"] == "" else None,
-                    int(bool(row["isOriginalTitle"])) if not pd.isna(row["isOriginalTitle"]) else None,
-                    # Check if the title is the primary for this movie to populate isPrimary
-                    int((row["mid"], row["title"]) in primaryLUT) 
+                    row["attributes"] if not pd.isna(row["attributes"]) and not row["attributes"] == "" else None
                 ))
-
                 insert_cnt += 1
             except sqlite3.IntegrityError as e:
                 # Skip invalid rows
@@ -462,10 +671,10 @@ def import_principals(conn):
         for _, row in principalsDf.iterrows():
             try:
                 c.execute("""
-                    INSERT INTO principals (movie_id, ordering, person_id, profession_id, category)
-                    VALUES (?, ?, 
-                        (SELECT profession_id FROM professions WHERE jobName = ? COLLATE NOCASE LIMIT 1),
-                    ?, ?)
+                    INSERT INTO principals (movie_id, ordering, person_id, profession_id, job)
+                    VALUES (?, ?, ?,
+                        (SELECT profession_id FROM professions WHERE job_name = ? COLLATE NOCASE LIMIT 1),
+                    ?)
                 """, (
                     row["mid"],
                     row["ordering"],
@@ -489,15 +698,28 @@ def import_principals(conn):
 
 
 with sqlite3.connect(DB_PATH) as conn:
+    # No dependencies
     import_movies(conn)
     import_persons(conn)
     import_genres(conn)
     import_characters(conn)
     import_professions(conn)
-    import_ratings(conn)
-    import_movie_genres(conn)
     import_titles(conn)
+
+    # depends on movies
+    import_ratings(conn)
+
+    # depends on movies and genres
+    import_movie_genres(conn)
+    # depends on movies and titles
+    import_movie_title(conn)
+    # depends on persons and professions
     import_person_profession(conn)
+
+    # depends on movie_titles
+    import_title_ordering(conn)
+
+    # depends on persons, movies, professions and characters
     import_knownformovies(conn)
     import_cast(conn)
     import_principals(conn)
